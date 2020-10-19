9afe39e3aa7b73a7f5b48a03ff71b241295bb0cb
Daniela Amaral <danielarosa2309@gmail.com>
2020-10-19 12:23:15 -0300

    Idempotencia

--- src/main/java/com/br/ages/calculadoraback/repository/AnualResultRepository.java
+++ src/main/java/com/br/ages/calculadoraback/repository/AnualResultRepository.java
@@ -2,12 +2,12 @@ package com.br.ages.calculadoraback.repository;
 
 import com.br.ages.calculadoraback.entity.AnualResultEntity;
 import com.br.ages.calculadoraback.entity.AnualResultPK;
-import com.br.ages.calculadoraback.entity.CoopProdPK;
-import com.br.ages.calculadoraback.entity.CooperativeProductEntity;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
+import java.util.Optional;
 
 public interface AnualResultRepository extends CrudRepository<AnualResultEntity, Long> {
 		AnualResultEntity findByAnualResultPK(AnualResultPK anualResultPK);
+
+		Optional<AnualResultEntity> findByAnualResultPK_IdCoop_CodCoopAndAnualResultPK_Year(String codCoop, Long year);
 }
